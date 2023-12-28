6b9e26415c4b4c4fbfcb9ca460b85c300bd1ccba
Ana Carolina <carolinamatosinhos98@gmail.com>
2023-12-28 16:37:19 -0300

    simple counter with vanilla JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("sub").addEventListener("click", subtraction)
 document.getElementById("add").addEventListener("click", addition)
-
+document.getElementById("reset").addEventListener("click", reset)
 
 function subtraction() {
   let value = document.getElementById("cont").innerText
@@ -12,4 +12,8 @@ function addition() {
   let value = document.getElementById("cont").innerText
   value = parseInt(value) + 1
   document.getElementById("cont").innerText = value
+}
+
+function reset() {
+  let value = document.getElementById("cont").innerText = 0
 }
